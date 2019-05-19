d7cfb696dd1cd50c64b33273bc9827895cddc095
github username <github email address>
2019-05-19 12:42:45 +0100

    Done.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,6 +1,4 @@
-# If you write a describe block without quotes like this, you need to define
-# the described class (or method) for the tests to even run. It's sort of a
-# freebie test to make sure you've actually defined the models you plan on
+# If you write a describe block without quotes like this, you need to define the described class (or method) for the tests to even run. It's sort of a freebie test to make sure you've actually defined the models you plan on
 # testing.
 
 describe Person do
